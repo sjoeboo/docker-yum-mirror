349332c3f9c7d97219b017328c79be0cf51f8840
Matt Nicholson <mnicholson@spotify.com>
2018-11-29 15:38:20 -0500

    b

--- trigger_docker_hub.sh
+++ trigger_docker_hub.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+TRAVIS_TAG=$1
 
 curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "$TRAVIS_TAG"}' -X POST https://registry.hub.docker.com/u/sjoeboo/docker-yum-mirror/trigger/$DOCKER_KEY/
